90b07ce8e1ef9f8fa412d200db2f472bdcfab4ba
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-08 19:37:26 +0300

    add NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link, Outlet} from "react-router-dom"
+export default function NavBar() {
+  return (
+    <div>
+        <div className='nav'>
+            {/* <Link to="/">Landing Page</Link> */}
+            {/* <div>
+                <Link to="/home">Home</Link>
+            </div> */}
+        </div>
+        <Outlet/>
+    </div>
+  )
+}
