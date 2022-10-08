2a9fc6c731b240348a802f5e9204357ef322fbb5
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-08 19:36:13 +0300

    add data and states

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
 import Home from "./components/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Landing from './components/Landing';
+// import NavBar from './components/NavBar';
+import { useState } from 'react';
+import { defaultUser } from './components/User';
 
 function App() {
+  
+    const [user, setUser] = useState(defaultUser.name)
+    const [password, setPassword] = useState(defaultUser.password)
+    const [email, setEmail] = useState(defaultUser.email)
+
+    const User = {user, email, password}
+
+    function signUp(newuser, newemail, newpassword){
+        setEmail(newemail)
+        setPassword(newpassword)
+        setUser(newuser)
+    }
+
+    // console.log(User)
+
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <div className='home'>
+        {/* <NavBar/> */}
+        <Routes>
+          <Route path='/' element={<Landing signUp={signUp} User={User}/>}/>
+          <Route path='home' element={<Home User={User}/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
