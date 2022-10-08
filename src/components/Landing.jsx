dd95c143f1c1a92454f52a3622dec09434134f51
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-08 19:36:30 +0300

    update details

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { useState } from 'react'
+import Login from './Login'
+import SignUp from './SignUp'
 
-export default function Landing({user}) {
-  return (
-    <div>
-        <h1>Welcome <span>{user.user}</span></h1>
-    </div>
-  )
+export default function Landing({ signUp, User }) {
+    const [hasAccount, setHasAccount] = useState(true)
+    function handleClick(){
+      setHasAccount(!hasAccount)
+    }
+    
+    return (
+      <div> 
+        {hasAccount ? <Login title="Login" handleClick={handleClick} User={User}/> : <SignUp handleClick={handleClick} signUp={signUp}/>}
+      </div>
+    )
 }
 
