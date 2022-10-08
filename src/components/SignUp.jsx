3b0c5b45961e60f0e553ac2ee15002fd75fbece2
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-08 13:06:54 +0300

    handle sign up logic

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { useState} from 'react'
 
-export default function SignUp({handleClick}) {
-  return (
-    <form>
-        <h2>Sign Up</h2>
-        <div className="input-control">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
-        </div>    
-        <div className="input-control">
-            <label htmlFor="password">Password</label>
-            <input type="password" placeholder='type password' name='password'/>
-        </div>
-        <div className="input-control">
-            <label htmlFor="password">Confirm Password</label>
-            <input type="password-2" placeholder='confirm password' name='password'/>
-        </div>
-        <div className="input-control">
-            <button type="submit">Sign Up</button>
-        </div>
-        <div className="input-control">
-            <p onClick={handleClick}>Already have an account?</p>
-        </div>
-    </form>
-  )
+export default function SignUp({handleClick, signUp}) {
+    const [username, setUserName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleSignUp(event){
+        event.preventDefault()
+        signUp(username, email, password)
+    }
+    
+    return (
+        <form onSubmit={handleSignUp}>
+            <h2>Sign Up</h2>
+            <div className="input-control">
+                <label htmlFor="name">Username</label>
+                <input type="text" name="username" id="username" value={username} onChange={(e) => setUserName(e.target.value)} required/>
+            </div>
+            <div className="input-control">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+            </div>    
+            <div className="input-control">
+                <label htmlFor="password">Create Password</label>
+                <input type="password" placeholder='type password' name='password' value={password} onChange={(e) => setPassword(e.target.value)} required/>
+            </div>
+            <div className="input-control">
+                <button type="submit">Sign Up</button>
+            </div>
+            <div className="input-control">
+                <p onClick={handleClick}>Already have an account?</p>
+            </div>
+        </form>
+    )
 }
