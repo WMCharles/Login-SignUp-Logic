871b289f74695e6078a14463d222d8966bf68e09
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-08 12:42:10 +0300

    add authentication logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Landing from './Landing'
+import { User as user } from '../User'
 
 export default function Login({handleClick, title}) {
-  return (
-    <form>
-        <h2>{title}</h2>
-        <div className="input-control">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
-        </div>    
-        <div className="input-control">
-            <label htmlFor="password">Password</label>
-            <input type="password" placeholder='type password' name='password'/>
+    const email = user.email
+    const password = user.password
+    const [inputEmail, setInputEmail] = useState("")
+    const [inputPassword, setInputPassword] = useState("")
+
+    function handleLogin(event){
+        event.preventDefault()
+        if(inputEmail === email && inputPassword === password){
+            alert(`Login successful! Welcome ${user.name}`)
+        } else {
+            alert("Invalid Credentials")
+        }
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleLogin}>
+                <h2>{title}</h2>
+                <div className="input-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="email" value={inputEmail} onChange={(e) => setInputEmail(e.target.value)}/>
+                </div>    
+                <div className="input-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder='type password' name='password' value={inputPassword} onChange={(e) => setInputPassword(e.target.value)}/>
+                </div>
+                <div className="input-control">
+                    <button type="submit">Login</button>
+                </div>
+                <div className="input-control">
+                    <p onClick={handleClick}>Don't have an account?</p>
+                </div>
+            </form>
+            <Landing/>
         </div>
-        <div className="input-control">
-            <button type="submit">Login</button>
-        </div>
-        <div className="input-control">
-            <p onClick={handleClick}>Don't have an account?</p>
-        </div>
-    </form>
-  )
+    )
 }
