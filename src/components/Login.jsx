fd33860e31ca4d7f97bdb90564c97d633fff7908
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-08 13:16:05 +0300

    update variables

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 import Landing from './Landing'
-import { User as user } from '../User'
+// import { User as user } from '../User'
 
-export default function Login({handleClick, title}) {
-    const email = user.email
-    const password = user.password
+export default function Login({handleClick, title, User}) {
+    const email = User.email
+    const password = User.password
     const [inputEmail, setInputEmail] = useState("")
     const [inputPassword, setInputPassword] = useState("")
 
     function handleLogin(event){
         event.preventDefault()
         if(inputEmail === email && inputPassword === password){
-            alert(`Login successful! Welcome ${user.name}`)
+            alert(`Login successful! Welcome ${User.user}`)
         } else {
             alert("Invalid Credentials")
         }
@@ -23,11 +23,11 @@ export default function Login({handleClick, title}) {
                 <h2>{title}</h2>
                 <div className="input-control">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" value={inputEmail} onChange={(e) => setInputEmail(e.target.value)}/>
+                    <input type="email" name="email" id="email" value={inputEmail} onChange={(e) => setInputEmail(e.target.value)} required/>
                 </div>    
                 <div className="input-control">
                     <label htmlFor="password">Password</label>
-                    <input type="password" placeholder='type password' name='password' value={inputPassword} onChange={(e) => setInputPassword(e.target.value)}/>
+                    <input type="password" placeholder='type password' name='password' value={inputPassword} onChange={(e) => setInputPassword(e.target.value)} required/>
                 </div>
                 <div className="input-control">
                     <button type="submit">Login</button>
