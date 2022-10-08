18e9f58facd9caa8b6dab94f90e56337394994bc
WMCharles <charles.wafula@student.moringaschool.com>
2022-10-08 19:35:28 +0300

    move to Landing

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,9 @@
-import React, { useState } from 'react'
-import Login from './Login'
-import SignUp from './SignUp'
-import { defaultUser } from '../User'
+import React from 'react'
 
-export default function Home() {
-    const [hasAccount, setHasAccount] = useState(true)
-    const [user, setUser] = useState(defaultUser.name)
-    const [password, setPassword] = useState(defaultUser.password)
-    const [email, setEmail] = useState(defaultUser.email)
-
-    const User = {user, email, password}
-
-    function handleClick(){
-        setHasAccount(!hasAccount)
-    }
-
-    function signUp(newuser, newemail, newpassword){
-        setEmail(newemail)
-        setPassword(newpassword)
-        setUser(newuser)
-    }
-
-    console.log(User)
-
+export default function Home({User}) {
     return (
-        <div className='home'>
-            {hasAccount ? <Login title="Login" handleClick={handleClick} User={User}/> : <SignUp handleClick={handleClick} signUp={signUp}/>}
+        <div>
+            <h1>Welcome <span>{User.user}</span></h1>
         </div>
     )
 }
